00031320d4b412973ac87cbc07d9e6a3188697c3
LAPEK\Dawid <wox0@interia.eu>
2017-03-24 22:36:38 +0100

    Last commit fix

--- RealDamCiPaczkeVRSymulator/Assets/Scripts/HandController.cs
+++ RealDamCiPaczkeVRSymulator/Assets/Scripts/HandController.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller {  get { return SteamVR_Controller.Input((int) trackedObject.index); } }
+    private SteamVR_TrackedObject trackedObject;
+
+   
+    void Start ()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+	void Update ()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initializerd");
+            return;
+        }
+        if (controller.GetPressDown(gripButton))
+        {
+            Debug.Log("Grip Button was just pressed");
+        }
+        if(controller.GetPressUp(gripButton))
+        {
+            Debug.Log("Grup button was just unpressed");
+        }
+     
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger enter");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger exit");
+    }
 }
