1baa7ccba677a2602551ed081d1e2c2615b592b1
LAPEK\Dawid <wox0@interia.eu>
2017-03-25 01:41:35 +0100

    Teleporting

---
+++ RealDamCiPaczkeVRSymulator/Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    public GameObject laserPrefab;
+    
+    private GameObject _laser;
+    
+    private Transform _laserTransform;
+    
+    private Vector3 _hitPoint;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    
+    void Update () {
+		
+	}
+}
