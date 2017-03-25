129df6be0ed420116c4e7e61071a340c2e3ffca7
LAPEK\Dawid <wox0@interia.eu>
2017-03-25 14:46:13 +0100

    cos

--- RealDamCiPaczkeVRSymulator/Assets/DollController.cs
+++ RealDamCiPaczkeVRSymulator/Assets/DollController.cs
@@ -14,6 +14,7 @@ public class DollController : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
+        if(other.gameObject.tag == "GameController")
         _yell.Play();
     }
 
