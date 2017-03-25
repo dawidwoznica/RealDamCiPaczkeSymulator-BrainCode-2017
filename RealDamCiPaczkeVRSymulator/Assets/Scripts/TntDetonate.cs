e61b4987f90309c823f3b0d6d0ad57cc2599869e
LAPEK\Dawid <wox0@interia.eu>
2017-03-25 14:07:45 +0100

    Tnt explosion

---
+++ RealDamCiPaczkeVRSymulator/Assets/Scripts/TntDetonate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TntDetonate : MonoBehaviour
+{
+
+
+    public GameObject explosion;
+
+    private float _areaOfEffect = 5f;
+    private Collider[] colls;
+    public LayerMask CanBeDamagedMask;
+    private AudioSource explosionSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        explosionSound = GetComponent<AudioSource>();
+
+        colls = Physics.OverlapSphere(transform.position, _areaOfEffect, CanBeDamagedMask);
+
+        for (int i = 0; i < colls.Length; i++)
+        {
+            Destroy(colls[i].gameObject);
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        explosionSound.Play();
+     
+        Destroy(explosion, 2f);
+    }
+}
